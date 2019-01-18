acfb53f801468f9f6323dba98f64c7498098a303
Jaime E. Valdez <jvaldezch@gmail.com>
2019-01-18 09:07:16 -0600

    Env changes

--- models/db.js
+++ models/db.js
@@ -10,8 +10,6 @@ var db = mysql.createPool({
     connectionLimit: 500
 });
 
-//const db = require('./.configdb.portal.js');
-
 const portalModel = {};
 
 portalModel.login = function (username, callback) {
@@ -52,18 +50,15 @@ portalModel.login = function (username, callback) {
                         connection.release();
 
                         if (err) callback({status: 'error',message: error}, null);
-
                         
                         if (rows.length > 0) {
                             for (var i = 0, len = rows.length; i < len; i++) {                
                                 user.bodegas[i] = rows[i].idBodega;
                             }
                         }
-
                         callback(null, user);
 
                     });
-
 
                 } else {
                     connection.release();
@@ -81,13 +76,23 @@ portalModel.login = function (username, callback) {
 
 portalModel.traficosDescarga = function (id_bodega, fecha, callback) {
 
-    if (db.getConnection() === undefined) {
-        callback("No database connection available", null);
-        return;
-    }
-
     db.getConnection(function (err, connection) {
-        var sql = "SELECT id, referencia, bultos, contenedorCajaEntrada AS caja_entrada FROM traficos WHERE idBodega = " + db.escape(id_bodega) + "AND fechaEta = " + db.escape(fecha) + ";";
+
+        if (err) {
+            callback({
+                status: 'error',
+                message: err
+            }, null);
+        }
+
+        var sql = "SELECT " +
+            "id, " +
+            "referencia, " +
+            "bultos, " +
+            "contenedorCajaEntrada AS caja_entrada " +
+            "FROM traficos " +
+            "WHERE idBodega = " + db.escape(id_bodega) + "AND fechaEta = " + db.escape(fecha) + ";";
+
         connection.query(sql, function (error, results, fields) {
             connection.release();
             if (error) {
@@ -95,18 +100,21 @@ portalModel.traficosDescarga = function (id_bodega, fecha, callback) {
                     status: 'error',
                     message: error
                 }, null);
+
             } else {
                 if (results.length > 0) {
                     callback(null, results);
                 } else {
                     callback({
                         status: 'No data found',
-                        message: 'User not found on database.'
+                        message: false
                     }, null);
                 }
             }
         });
+
     });
+
 }
 
 portalModel.bodegas = function (id_user, callback) {
@@ -120,7 +128,10 @@ portalModel.bodegas = function (id_user, callback) {
             }, null);
         }
 
-        var sql = "SELECT * FROM trafico_usubodegas WHERE idUsuario = " + id_user + ";";
+        var sql = "SELECT " +
+            "* " +
+            "FROM trafico_usubodegas " +
+            "WHERE idUsuario = " + id_user + ";";
 
         connection.query(sql, function (error, results, fields) {
             connection.release();
@@ -141,27 +152,37 @@ portalModel.bodegas = function (id_user, callback) {
                 }
             }
         });
+
     });
     
 }
 
 portalModel.detalleTrafico = function (id_trafico, callback) {
 
-    if (db.getConnection() === undefined) {
-        callback("No database connection available", null);
-        return;
-    }
-
     db.getConnection(function (err, connection) {
-        var sql = "SELECT t.idBodega as id_bodega,\
-        t.idCliente as id_cliente, t.referencia,\
-        t.rfcCliente AS rfc_cliente, c.nombre as nombre_cliente, t.bultos,\
-        t.ordenCarga AS orden_carga, t.blGuia as bl_guia,\
-        t.contenedorCajaEntrada AS caja_entrada,\
-        t.contenedorCaja as caja, t.lineaTransporte as linea_transporte \
-        FROM traficos t \
-        LEFT JOIN trafico_clientes c ON c.id = t.idCliente \
-        WHERE t.id = " + db.escape(id_trafico) + ";";
+
+        if (err) {
+            callback({
+                status: 'error',
+                message: err
+            }, null);
+        }
+
+        var sql = "SELECT " +
+            "t.idBodega as id_bodega," +
+            "t.idCliente as id_cliente, " +
+            "t.referencia, " +
+            "t.rfcCliente AS rfc_cliente, " +
+            "c.nombre as nombre_cliente, " +
+            "t.bultos, " +
+            "t.ordenCarga AS orden_carga, " +
+            "t.blGuia as bl_guia, " +
+            "t.contenedorCajaEntrada AS caja_entrada, " +
+            "t.contenedorCaja as caja, t.lineaTransporte as linea_transporte " +
+            "FROM traficos t " +
+            "LEFT JOIN trafico_clientes c ON c.id = t.idCliente " +
+            "WHERE t.id = " + db.escape(id_trafico) + ";";
+
         connection.query(sql, function (error, results, fields) {
             connection.release();
             if (error) {
@@ -169,6 +190,7 @@ portalModel.detalleTrafico = function (id_trafico, callback) {
                     status: 'error',
                     message: error
                 }, null);
+
             } else {
                 if (results.length > 0) {
                     callback(null, results);
@@ -180,7 +202,9 @@ portalModel.detalleTrafico = function (id_trafico, callback) {
                 }
             }
         });
+    
     });
+
 }
 
 module.exports = portalModel;
